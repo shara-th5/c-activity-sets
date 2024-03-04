5989e58c696a2309ed4994ebd42c1da9336cf617
shara-th5 <144095005+shara-th5@users.noreply.github.com>
2024-03-04 14:30:09 +0530

    test

---
+++ set01/problem12.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+struct _complex 
+{
+    float real;
+    float imaginary;
+};
+typedef struct _complex Complex;
+
+int get_n() 
+{
+    int n;
+    printf("Enter the number of complex numbers (n): ");
+    scanf("%d", &n);
+    return n;
+}
+
+Complex input_complex() 
+{
+    Complex x;
+    printf("Enter the real part: ");
+    scanf("%f", &x.real);
+    printf("Enter the imaginary part: ");
+    scanf("%f", &x.imaginary);
+    return x;
+}
+
+void input_n_complex(int n, Complex c[n]) 
+{
+    for (int i = 0; i < n; i++) 
+    {
+        printf("Enter complex number %d:\n", i + 1);
+        c[i] = input_complex();
+    }
+}
+
+Complex add(Complex a, Complex b) 
+{
+    Complex sum;
+    sum.real = a.real + b.real;
+    sum.imaginary = a.imaginary + b.imaginary;
+    return sum;
+}
+
+
+Complex add_n_complex(int n, Complex c[n]) 
+{
+    Complex sum = {0, 0}; 
+    for (int i = 0; i < n; i++) 
+    {
+        sum = add(sum, c[i]);
+    }
+    return sum;
+}
+
+void output(int n, Complex c[n], Complex result) 
+{
+    printf("Sum of %d complex numbers:\n", n);
+
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%f + %fi ", c[i].real, c[i].imaginary);
+        if (i < n - 1) {
+            printf("+ ");
+        }
+    }
+    printf("= %f + %fi\n", result.real, result.imaginary);
+}
+
+int main() 
+{
+    int n;
+    n = get_n(); 
+    Complex numbers[n];
+    input_n_complex(n, numbers);
+    Complex result = add_n_complex(n, numbers);
+    output(n, numbers, result);
+    return 0;
+}
